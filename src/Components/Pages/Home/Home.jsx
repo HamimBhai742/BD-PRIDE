176be32ec8d4f263733027a231e8529a334fc9a8
HamimBhai742 <omagootruelove147@gmail.com>
2024-07-12 19:19:33 +0600

    modified

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
+import MainContent from './MainContent/MainContent';
+import Content from './MainContent/Content';
+import Slider from './Slider/Slider';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <MainContent></MainContent>
+            <Content></Content>
+            <Slider></Slider>
         </div>
     );
 };
